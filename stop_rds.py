c5f3d8052b4c8d17693780e8b9c63b746ed5ecff
easec <mats.johannesson@easec.se>
2022-09-26 14:27:36 +0200

    Create stop_rds.py

---
+++ stop_rds.py
@@ -0,0 +1,26 @@
+import boto3
+rds = boto3.client('rds')
+
+def lambda_handler(event, context):
+
+    #Stop DB instances
+    dbs = rds.describe_db_instances()
+    for db in dbs['DBInstances']:
+        #Check if DB instance is not already stopped
+        if (db['DBInstanceStatus'] == 'available'):
+            DoNotStop=1
+            try:
+                GetTags=rds.list_tags_for_resource(ResourceName=db['DBInstanceArn'])['TagList']
+                for tags in GetTags:
+                #if tag "autostop=yes" is set for instance, stop it
+                    if(tags['Key'] == 'autostop' and tags['Value'] == 'yes'):
+                        result = rds.stop_db_instance(DBInstanceIdentifier=db['DBInstanceIdentifier'])
+                        print ("Stopping instance: {0}.".format(db['DBInstanceIdentifier']))
+                if(DoNotStop == 1):
+                    DoNotStop=1
+            except Exception as e:
+                print ("Cannot stop instance {0}.".format(db['DBInstanceIdentifier']))
+                print(e)
+                
+if __name__ == "__main__":
+    lambda_handler(None, None)
